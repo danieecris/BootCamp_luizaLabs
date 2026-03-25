91fc2297e0ac33b27f918e1a7e6d8606b840cd83
danieecris <danielolicristian12@gmail.com>
2026-03-24 22:44:37 -0300

    BootCamp DIO  - LuizaLabs 3

---
+++ Aula_15/while_for.py
@@ -0,0 +1,16 @@
+texto = input("inform um texto: ")
+VOGAIS = "AEIOU"
+
+for letra in texto:
+    
+    if letra.upper() in VOGAIS:
+        print(letra)
+
+print()        
+
+            
+
+
+
+
+
