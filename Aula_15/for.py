68723bcc7be88c393e07cc310c707e3ec9bb9f99
danieecris <danielolicristian12@gmail.com>
2026-03-25 15:10:33 -0300

    Estruturas de repeticao | for e while

---
+++ Aula_15/for.py
@@ -0,0 +1,28 @@
+#for
+
+texto = input("inform um texto: ")
+VOGAIS = "AEIOU"
+
+for letra in texto:
+    
+    if letra.upper() in VOGAIS:
+        print(letra)
+
+else:
+    print("Seu codigo terminou aqui \n")        
+
+
+print(list(range(4)))
+
+            
+for i in range(1,10):
+    print(i)    
+
+
+for num in range(0, 11, 2):
+    print(f"{num} \n")
+
+
+
+#while
+
