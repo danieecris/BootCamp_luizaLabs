e3f1ef7020d886d7c1ab891745c102b7b729cb1c
danieecris <danielolicristian12@gmail.com>
2026-03-27 10:51:06 -0300

    strings multiplas - BootCamp Dio | Luiza Labs

---
+++ Aula_19/string_multiplas.py
@@ -0,0 +1,16 @@
+nome = "Daniel"
+mensagem =f"""
+Ola meu nome e {nome}
+    Estou estudando python
+"""
+
+print (mensagem)
+
+
+print("""
+        ================= menu ==================
+        1 - Depositar
+        2 - Sacar
+        0 - sair
+      
+      """)
